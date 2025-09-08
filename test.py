51684ddb012364824cf13e925b24e198e0deb4ad
SeanHgithub <seanhadley11@gmail.com>
2025-09-08 10:23:13 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,4 +7,14 @@ t.shape('turtle')
 
 t.forward(200)
 
+def square(x)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
+
 turtle.done()
